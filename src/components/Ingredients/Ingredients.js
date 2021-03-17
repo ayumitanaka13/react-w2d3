08c17217064fa0cbe68462164cf4e4284aaf0870
ayumitanaka <tanakayumi13@gmail.com>
2021-03-17 18:08:49 +0000

    modified Ingredients.js

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-import IngredientForm from './IngredientForm';
-import Search from './Search';
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
 
 function Ingredients() {
+  const [userIngredients, setUserIngredients] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://react-hooks-98ff6-default-rtdb.firebaseio.com/ingredients.json"
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amout: responseData[key].amount,
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log("Rendering", userIngredients);
+    return () => {
+      console.log("This component will be unmounted");
+    };
+  }, [userIngredients]);
+
+  const addIngredientHandler = (ingredients) => {
+    fetch(
+      "https://react-hooks-98ff6-default-rtdb.firebaseio.com/ingredients.json",
+      {
+        method: "POST",
+        body: JSON.stringify(ingredients),
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log(responseData);
+        setUserIngredients((prevState) => [
+          ...prevState,
+          { id: responseData.name, ...ingredients },
+        ]);
+      });
+  };
+
+  const removeIngredientHandler = (ingredientId) => {
+    setUserIngredients((prevState) => {
+      prevState.filter((ingredient) => ingredient.id !== ingredientId);
+    });
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
       </section>
     </div>
   );
